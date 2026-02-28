634aabf3eab838604cb204082e0d84c895dc1b9e
anujmaharjan10 <anuj.maharjan@jeeveehealth.com.np>
2026-02-28 13:12:58 +0545

    added follow model

---
+++ followers/models.py
@@ -0,0 +1,22 @@
+from datetime import datetime
+import uuid
+from database import db
+
+class Follow(db.Model):
+    id = db.Column(db.String(), primary_key=True, default=lambda: str(uuid.uuid4())) #initiating FOLLOW
+    from_user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)   #the one being followed
+    to_user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.now())
+
+    __table_args__ = (
+        db.UniqueConstraint("from_user_id", "to_user_id", name="unique_follow"),
+    )
+
+    follower = db.relationship("User", foreign_keys=[from_user_id], backref="following_records") #who i follow
+    following = db.relationship("User", foreign_keys=[to_user_id], backref="follower_records") #who follows me
+
+    def __repr__(self):
+        return f"<Follow from={self.from_user_id} to={self.to_user_id}>"
+    
+    
+    
